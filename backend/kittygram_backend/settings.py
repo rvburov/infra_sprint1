c1f8f41d50872b2a528723be644ec61ac9f27c3a
rvburov <romanburov89@gmail.com>
2024-02-13 00:32:21 +0000

    my commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.23.101', '127.0.0.1', 'localhost', 'peepingcent.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,7 +90,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
