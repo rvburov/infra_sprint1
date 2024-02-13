d00f21eb168fc9a019045160f44f5753883c1926
rvburov <romanburov89@gmail.com>
2024-02-13 01:16:07 +0000

    my commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.23.101', '127.0.0.1', 'localhost', 'peepingcent.hopto.org']
 
