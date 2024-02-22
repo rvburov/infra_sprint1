880c1386093f9890de2109f817b8c54a9a6fadb3
rvburov <romanburov89@gmail.com>
2024-02-22 00:04:11 +0000

    my commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = ('E=Q}/-E{IprV&AD2H&>iYI-i|w]!uJ{@,94p!hIB[q]}WpPrHq')
+SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
 
 DEBUG = False
 
